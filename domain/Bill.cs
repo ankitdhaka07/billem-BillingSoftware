f43f4b3536d739f3d2d937c227176ba5ca6e8b28
ankitdhaka <ankit.dhaka@clrslate.io>
2026-03-12 00:37:47 +0530

    Modify domains to include more details and new ent

--- domain/Bill.cs
+++ domain/Bill.cs
@@ -1,10 +1,11 @@
 ﻿public class Bill
 {
     public Guid Id { get; set; }
-    public List<BillItem> BillItems { get; set; } = new();    
+    public List<BillItem> BillItems { get; set; } = new();
+    public DateTime Date { get; set; } = DateTime.Now;
     public double TotalTaxableAmount  => BillItems.Sum(billItem => billItem.Item.Price * billItem.Quantity);
     public double TaxPercentage = 0.05;
     public double TaxApplied => TotalTaxableAmount * TaxPercentage;
     public double TotalAmount => TotalTaxableAmount * 1.05;
-
+    public string VehicleNumber { get; set; }
 }
---
+++ domain/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace domain;
+
+public class Customer
+{
+    public string Id { get; set; }
+    [Required] public string Name { get; set; }
+    public string? GstNo { get; set; }
+    public string? ShippingAddress { get; set; }
+    public string? BillingAddress { get; set; }
+
+}
